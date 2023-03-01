625ae70872443fce0e45f5c32edffe639af4adeb
o5v3 <oadv310@hotmail.com>
2023-03-01 00:36:07 -0400

    Add primevue integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    typescript: {
+        shim: false
+    },
+    css: [
+        "primevue/resources/themes/vela-orange/theme.css",
+        "primevue/resources/primevue.css",
+        "primeicons/primeicons.css"
+    ],
+    build: {
+        transpile: ["primevue"]
+    }
 })
