5b3089eefe01c22bad683675a5b0957bc704e300
o5v3 <omardavidelfuerte@hotmail.com>
2023-03-09 18:52:17 -0400

    Test Nuxt's capabilities

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modules: [
+        "@nuxt/image-edge"
+    ],
     typescript: {
         shim: false
     },
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+const images = [
+  "gitlab-logo-500.png",
+  "gitlab-logo-600.png",
+  "gitlab-logo-700.png",
+]
+
+const image_id = ref(0)
+const image = computed(() => images[image_id.value])
+
+</script>
+
+<template>
+  <div>
+    <NuxtWelcome />
+    <nuxt-img :src="'/images/test/' + image" />
+    <button @click="image_id = image_id == 0 ? 2 : image_id - 1 % 3">Previous</button>
+    <button @click="image_id = image_id + 1 % 3">Next</button>
+  </div>
+</template>
+
+<style scoped></style>
