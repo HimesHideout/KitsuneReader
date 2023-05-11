5220e1bbd5a0ada08f53b568897b69d12161dafc
Yellowtail01 <calmhamachi@gmail.com>
2023-05-11 12:12:38 -0700

    Added Landing, Cover, and Messages Pages with navigation

---
+++ pages/messages.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div>
+    <NuxtLink to="/" id="link-back-messages">
+      <Button icon="pi pi-angle-left" class="forefront" text></Button>
+    </NuxtLink>
+    <!-- Need to connect to database -->
+    <ul>
+        <li>Place holder for images</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
