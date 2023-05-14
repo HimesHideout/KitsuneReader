ef3ff71ebad7ed2819ea0616653ac2e842270b5d
o5v3 <oadv310@hotmail.com>
2023-05-13 20:47:44 -0400

    Setup layout of cover page to match sketch

--- pages/cover.vue
+++ pages/cover.vue
@@ -1,27 +1,32 @@
 <script setup></script>
 
 <template>
-  <div>
+  <div class="h-screen">
     <header>
-      <NuxtLink to="/" id="link-back-cover">
+      <NuxtLink to="/" id="link-back-cover" class="absolute">
           <Button icon="pi pi-angle-left" class="forefront" text></Button>
       </NuxtLink>
+      <div class="flex align-items-center justify-content-around">
+        <h2>ZERO CARDS, OVER</h2>
+      </div>
     </header>
-    <div class="main_card">
-      <Card>
-        <template #title>
-          <p>Happy Birthday Hime!</p>
-        </template>
-        <template #content>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam incidunt architecto voluptates inventore, dignissimos eveniet soluta debitis sunt neque minima porro cumque alias sequi asperiores tempora laborum, harum dolore ab.
-          </p>
-        </template>
-      </Card>
-    </div>
-    <div class="flex align-items-center justify-content-around low_buttons">
-        <NuxtLink to="/reader/1"><Button label="Reader page" /></NuxtLink>
-        <NuxtLink to="/chapters"><Button label="Chapters list" /></NuxtLink>
+    <div class="flex flex-column justify-content-around content">
+      <div class="main_card flex align-items-center mx-8 my-4">
+        <Card>
+          <template #title>
+            <p>Happy Birthday Hime!</p>
+          </template>
+          <template #content>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam incidunt architecto voluptates inventore, dignissimos eveniet soluta debitis sunt neque minima porro cumque alias sequi asperiores tempora laborum, harum dolore ab.
+            </p>
+          </template>
+        </Card>
+      </div>
+      <div class="flex flex-wrap align-items-center justify-content-around low_buttons">
+          <NuxtLink to="/reader/1" class="low_button"><Button label="Reader page" /></NuxtLink>
+          <NuxtLink to="/chapters" class="low_button my-2 sm:my-0"><Button label="Chapters list" /></NuxtLink>
+      </div>
     </div>
   </div>
 </template>
@@ -32,10 +37,18 @@ header {
 }
 
 .main_card {
-  height: 70%;
+  height: 60%;
+}
+
+.content {
+  height: 80%;
 }
 
 .low_buttons {
   height: 10%;
+}
+
+.low_button {
+  min-width: 7em;
 }
 </style>
