395bb410bc74a70e073d8347c9147fbc5882347d
Yellowtail01 <128086230+Yellowtail01@users.noreply.github.com>
2023-06-01 12:07:19 -0700

    Fixed need for refresh on messages page for tilt

--- pages/messages.vue
+++ pages/messages.vue
@@ -1,7 +1,15 @@
 <script setup>
 import VanillaTilt from 'vanilla-tilt';
+const messageRefs = ref([]);
 let {data: messages} = await useFetch("/api/messages")
-
+onMounted(()=>{
+  for(let reference of messageRefs.value){
+    VanillaTilt.init(reference.$el, {
+      max: 2,
+      scale: 1.15
+    });
+  }  
+})
 </script>
 
 <template>
@@ -15,19 +23,17 @@ let {data: messages} = await useFetch("/api/messages")
         </div>
     </header>
     <div>
-      <!-- Need to connect to database -->
       <div class="flex flex-column align-items-center justify-content-around">
 <!--         <Skeleton v-for="message in messages" width="50%" height="15rem" class="m-5" data-tilt data-tilt-max="15" data-tilt-scale="1.2"/>
  -->        
       <nuxt-img v-for="message in messages"
+      ref="messageRefs"
       :src="`/superchats/${message['name']}`" 
       quality="100" 
       class="m-5 w-9 sm:w-6" 
       format="png"
       loading="lazy"
-      data-tilt 
-      data-tilt-max="2" 
-      data-tilt-scale="1.15" />
+      />
       </div>
     </div>
   </div>
