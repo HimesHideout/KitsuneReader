b200b1fb8575f1a57315c6ed84a67d801fb74c96
Yellowtail01 <128086230+Yellowtail01@users.noreply.github.com>
2023-06-03 11:04:50 -0700

    Add default site title and preload to chapter images

--- app.vue
+++ app.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+// Add into <script setup> on individual pages.
+// Title will then be whatever is 'Title - Kitsune Reader'
+// Default is Kitsune Reader if this isn't added to the <script setup>
+// useHead({
+//   title: ''
+// })
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - Kitsune Reader` : 'Kitsune Reader';
+  }
+})
+</script>
+
 <template>
   <div>
     <NuxtPage />
