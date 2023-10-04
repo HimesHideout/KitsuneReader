6d832fefd1e5b05f9cb377f2d4e26d362505cd9f
o5v3 <oadV310@hotmail.com>
2023-10-04 15:01:22 +0000

    Change messages layout to have irregular rows

--- pages/anniversaryMessages.vue
+++ pages/anniversaryMessages.vue
@@ -2,6 +2,15 @@
 import VanillaTilt from 'vanilla-tilt';
 const messageRefs = ref([]);
 let {data: messages} = await useFetch("/api/messages")
+
+let columns = [[], [], []]
+
+let column = 0
+for (let message of messages.value) {
+  columns[column].push(message)
+  column = (column + 1) % 3
+}
+
 onMounted(()=>{
   for(let reference of messageRefs.value){
     VanillaTilt.init(reference.$el, {
@@ -22,20 +31,30 @@ onMounted(()=>{
           <h2>ANNIVERSARY MESSAGES</h2>
         </div>
     </header>
-    <div>
-      <div class="flex flex-row flex-wrap align-items-center justify-content-evenly gap-1">
-       
-      <nuxt-img v-for="message in messages"
-      ref="messageRefs"
-      :src="`/superchats/${message['name']}`" 
-      quality="100" 
-      class="m-5 w-3 flex-grow-1 flex-shrink-1" 
-      format="png"
-      loading="lazy"
-      />
+    <div class="flex flex-column md:flex-row">
+      <div class="flex flex-column" v-for="n in 3">
+          <nuxt-img v-for="message in columns[n - 1]"
+          ref="messageRefs"
+          :src="`/superchats/${message['name']}`" 
+          quality="100" 
+          class="image-item m-3 w-10 align-self-center" 
+          format="png"
+          loading="lazy"
+          />
       </div>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.image-gallery {
+  /* Mobile first */
+  flex-direction: column;
+}
+
+@media only screen and (min-width: 768px) {
+  .image-gallery {
+    flex-direction: row;
+  }
+}
+</style>
