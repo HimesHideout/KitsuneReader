e3d9a01b3698db04594e8a3d1a8828f26141a615
Yellowtail01 <128086230+Yellowtail01@users.noreply.github.com>
2023-10-08 16:59:15 -0700

    Adjust landing page layout p2

--- pages/index.vue
+++ pages/index.vue
@@ -2,56 +2,73 @@
 
 <template>
   <div class="h-screen">
-    <div class="flex align-items-center justify-content-around">
-      <h2>2 YEAR ANNIVERSARY</h2>
-    </div>
-    <div class="flex flex-wrap align-items-center justify-content-around content mb-4">
-      <Card class="w-5 mb-8 overflow-auto" id="AnniversaryCard">
-        <!--Change the logo here-->
-        <template #header>
-          <img src="~/assets/logos/Superchat_Logo.png" style="display: block; margin-top: 2em; margin-left: auto; margin-right: auto; max-width: 80%; max-height: 70%; "/>
-        </template>
-        <template #title>
-          <p class="lg:text-3xl sm:text-2xl text-center no-underline">Anniversary messages from the kits in the community!</p>
-        </template>
-        <template #content>
-          <div class="flex justify-content-center">
-            <NuxtLink to="/anniversaryMessages"><Button label="Go" id="AnniversaryButton"/></NuxtLink>
-          </div>
-        </template>
-      </Card>
-    </div>
-    <div class="flex align-items-center justify-content-around">
-      <h2>HIME BIRTHDAY PROJECT 2023</h2>
-    </div>
-    <div class="flex flex-wrap align-items-center justify-content-around content mb-8">
-      <Card class="w-5 mb-8 overflow-auto" id="MangaCard">
-        <template #header>
-          <img src="~/assets/logos/Zero_Cards_Over_Logo.png" style="display: block; margin-top: 2em; margin-left: auto; margin-right: auto; max-width: 55%; max-height: 60%; "/>
-        </template>
-        <template #title>
-          <p class="lg:text-3xl text-2xl text-center">A Collaborative Manga Project, put together by the talented writers and artists in Hime's Hideout</p>
-        </template>
-        <template #content>
-          <div class="flex flex-wrap justify-content-around gap-3 align-content-center">
-            <NuxtLink to="/reader/0#1" class=""><Button label="Start Reading" :external="true" class="sm:text-base md:text-xl p-3 no-underline" id="ReadingButton"/></NuxtLink>
-            <NuxtLink to="/chapters" class=""><Button label="Chapter Select" class="sm:text-base md:text-xl p-3 no-underline" id="ChaptersButton"/></NuxtLink>
-          </div>
-        </template>
-      </Card>
-      <Card class="w-5 mb-8 overflow-auto" id="MessagesCard">
-        <template #header>
-          <img src="~/assets/logos/Superchat_Logo.png" style="display: block; margin-top: 2em; margin-left: auto; margin-right: auto; max-width: 80%; max-height: 70%; "/>
-        </template>
-        <template #title>
-          <p class="lg:text-3xl sm:text-2xl text-center no-underline">Birthday messages from the kits in the community!</p>
-        </template>
-        <template #content>
-          <div class="flex justify-content-center">
-            <NuxtLink to="/messages"><Button label="Go" id="MessagesButton"/></NuxtLink>
-          </div>
-        </template>
-      </Card>
+    <div class="flex flex-column align-items-center justify-content-evenly">
+      <div class="flex align-items-center justify-content-center">
+        <h2>2 YEAR ANNIVERSARY</h2>
+      </div>
+      <div class="flex flex-wrap justify-content-evenly align-items-center mb-5 gap-4">
+        <div class="card flex-column justify-content-center">
+          <Image src="/images/art/CyberNatasha_2nd_Anniversary.png"
+              image-class="h-27rem w-12"
+              preview
+          ></Image>
+          <div>Art by: CyberNatasha</div>
+        </div>
+        <Card class="w-4 h-25rem overflow-auto" id="AnniversaryCard">
+          <!--Uncomment and change the logo here-->
+          <!-- <template #header>
+            <img src="~/assets/logos/Superchat_Logo.png" style="display: block; margin-top: 2em; margin-left: auto; margin-right: auto; max-width: 80%; max-height: 70%; "/>
+          </template> -->
+          <template #title>
+            <p class="lg:text-4xl sm:text-2xl text-center no-underline">Anniversary messages from the kits in the community!</p>
+          </template>
+          <template #content>
+            <div class="flex justify-content-center">
+              <NuxtLink to="/anniversaryMessages"><Button label="Go" id="AnniversaryButton"/></NuxtLink>
+            </div>
+          </template>
+        </Card>
+        <div class="card flex-column align-items-center justify-content-center">
+          <Image src="/images/art/AlexC_2nd_Anniversary.png"
+              image-class="h-21rem w-12"
+              preview
+              
+          ></Image>
+          <div>Art by: Alex Chan</div>
+        </div>
+      </div>
+      <div class="flex align-items-center justify-content-center">
+        <h2>HIME BIRTHDAY PROJECT 2023</h2>
+      </div>
+      <div class="flex flex-wrap align-items-center justify-content-around">
+        <Card class="w-5 mb-8 overflow-auto" id="MangaCard">
+          <template #header>
+            <img src="~/assets/logos/Zero_Cards_Over_Logo.png" style="display: block; margin-top: 2em; margin-left: auto; margin-right: auto; max-width: 55%; max-height: 60%; "/>
+          </template>
+          <template #title>
+            <p class="lg:text-3xl text-2xl text-center">A Collaborative Manga Project, put together by the talented writers and artists in Hime's Hideout</p>
+          </template>
+          <template #content>
+            <div class="flex flex-wrap justify-content-around gap-3 align-content-center">
+              <NuxtLink to="/reader/0#1" class=""><Button label="Start Reading" :external="true" class="sm:text-base md:text-xl p-3 no-underline" id="ReadingButton"/></NuxtLink>
+              <NuxtLink to="/chapters" class=""><Button label="Chapter Select" class="sm:text-base md:text-xl p-3 no-underline" id="ChaptersButton"/></NuxtLink>
+            </div>
+          </template>
+        </Card>
+        <Card class="w-5 mb-8 overflow-auto" id="MessagesCard">
+          <template #header>
+            <img src="~/assets/logos/Superchat_Logo.png" style="display: block; margin-top: 2em; margin-left: auto; margin-right: auto; max-width: 80%; max-height: 70%; "/>
+          </template>
+          <template #title>
+            <p class="lg:text-3xl sm:text-2xl text-center no-underline">Birthday messages from the kits in the community!</p>
+          </template>
+          <template #content>
+            <div class="flex justify-content-center">
+              <NuxtLink to="/messages"><Button label="Go" id="MessagesButton"/></NuxtLink>
+            </div>
+          </template>
+        </Card>
+      </div>
     </div>
   </div>
 </template>
